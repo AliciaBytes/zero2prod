bd5405fb89b8e14523e215bde67a07aefd1ba3e9
Alicia Miedl <alicia@aliciabytes.dev>
2022-11-02 08:04:12 +0100

    Add health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/health_check", get(health_check));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
+
+async fn health_check() {}
