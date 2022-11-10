d8f2d68b80a7dc662ee08f16f0b44c57b7ce06f9
Alicia Miedl <alicia@aliciabytes.dev>
2022-11-10 09:42:28 +0100

    Create a preliminary email subscription endpoint.
    - Add dummy endpoint to subscribe to the email newsletter
    - Write tests to make sure it gives the correct http response

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,31 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, routing::post, Form, Router};
 
 #[must_use]
 pub fn app() -> Router {
     // build our application with a single route
-    Router::new().route("/health_check", get(health_check))
+    Router::new()
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe))
 }
 
 #[allow(clippy::unused_async)]
 async fn health_check() {}
+
+#[allow(clippy::unused_async)]
+async fn subscribe(Form(_input): Form<SubscriptionFormData>) {}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct SubscriptionFormData {
+    email: String,
+    name: String,
+}
+
+impl SubscriptionFormData {
+    #[must_use]
+    pub fn new(email: &str, name: &str) -> Self {
+        SubscriptionFormData {
+            email: email.to_string(),
+            name: name.to_string(),
+        }
+    }
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -19,3 +19,55 @@ async fn health_check() {
 
     assert_eq!(response.status(), StatusCode::OK);
 }
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let app = app();
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method("POST")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .uri("/subscriptions")
+                .body(Body::from("name=Le Guin&email=ursula_le_guin@gmail.com"))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app = app();
+
+    let test_cases = vec![
+        ("name=Le Guin", "missing the email"),
+        ("email=ursula_le_guin@gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = app
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .method("POST")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .uri("/subscriptions")
+                    .body(Body::from(invalid_body))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(
+            response.status(),
+            StatusCode::UNPROCESSABLE_ENTITY,
+            "The API did not fail with 422 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
